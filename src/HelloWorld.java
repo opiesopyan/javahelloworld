890a77b65148d870e376adc61b365d036006fef5
opiesopyan <opieryten@gmail.com>
2017-12-19 14:54:57 +0700

    Test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main (String [] args) {
-		System.out.println("Hello World again");
-		System.out.println("Opie Sopyan ganteng banget");
+		System.out.println("coba dulu");
+		System.out.println("Opie testing lieur Sopyan Testing");
 	}
 }
